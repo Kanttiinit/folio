fc95c9244970ee0af278731b3a4688dfc7dcb3c3
Max Huttunen <max.huttunen@gmail.com>
2019-02-09 14:35:58 +0200

    use graphql

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 const express = require("express");
 const twig = require("twig");
-const fetch = require("node-fetch");
 const moment = require("moment");
 const ua = require("universal-analytics");
 const minifyHTML = require("express-minify-html");
 const slugify = require("@sindresorhus/slugify");
 const url = require("url");
-const querystring = require("querystring");
+const { request } = require("graphql-request");
 
 const explainProperty = require("./explainProperty");
 const translate = require("./translate");
-
-const cache = new Map();
-
-const get = async (resource, ttl = 600) => {
-  const existing = cache.get(resource);
-  const now = Math.round(Date.now() / 1000);
-  if (existing && existing.expires > now) {
-    return existing.data;
-  }
-  const response = await fetch(`https://kitchen.kanttiinit.fi/${resource}`);
-  const data = await response.json();
-  cache.set(resource, { data, expires: now + ttl });
-  return data;
-};
 
 const app = express();
 
@@ -65,65 +50,94 @@ app
   .get("/restaurant", async (req, res) => {
     const { day = moment().format("YYYY-MM-DD"), id, lang = "fi" } = req.query;
     try {
-      const [restaurant] = await get(`restaurants?ids=${id}&lang=${lang}`);
-      const menus = await get(
-        `menus?restaurants=${id}&days=${day}&lang=${lang}`
+      const query = `
+        {
+          restaurant(id: ${id}, lang: ${lang}) {
+            name
+            menus(day: "${day}") {
+              courses {
+                title
+                properties
+              }
+            }
+          }
+        }
+      `;
+      const data = await request(
+        "https://kitchen.kanttiinit.fi/graphql",
+        query
       );
       res.render("restaurant", {
-        restaurant,
-        courses: menus[restaurant.id][day],
+        restaurant: data.restaurant,
         day,
-        title: restaurant.name + " Menus"
+        title: data.restaurant.name + " Menus"
       });
     } catch (e) {
       console.log(e);
-      res.status(500).render("index", { lang, error: "Failed to fetch data." });
+      res
+        .status(500)
+        .render("error", { lang, errorMessage: "Failed to fetch data." });
     }
   })
   .get("/robots.txt", (req, res) => res.send("User-agent: *\nDisallow:"))
   .get("/:lang/area/:areaId/:day?", async (req, res) => {
     const day = moment(req.params.day);
     const areaId = Number(req.params.areaId) || 1;
+    const date = day.format("YYYY-MM-DD");
 
     try {
-      const areas = await get("areas?idsOnly=1&lang=" + res.locals.lang, 3600);
-      const currentArea = areas.find(area => area.id === areaId);
-      if (!currentArea) {
-        res.status(400).render("area", { areas, error: "Invalid area." });
-      } else if (!day.isValid()) {
+      if (!day.isValid()) {
         res
           .status(400)
           .render("area", { areas, currentArea, error: "Invalid date." });
       } else {
-        const restaurantIds = currentArea.restaurants.join(",");
-        const restaurants = await get(
-          `restaurants?ids=${restaurantIds}&lang=${res.locals.lang}`,
-          3600
+        const query = `
+        {
+          areas(lang: ${res.locals.lang}) {
+            id
+            name
+          }
+          area(id: ${areaId}, lang: ${res.locals.lang}) {
+            id
+            name
+            restaurants {
+              name
+              address
+              url
+              menus(day: "${date}") {
+                courses {
+                  title
+                  properties
+                }
+              }
+            }
+          }
+        }
+      `;
+        const data = await request(
+          "https://kitchen.kanttiinit.fi/graphql",
+          query
         );
         const now = moment();
         const tomorrow = now.clone().add({ days: 1 });
-        const date = day.format("YYYY-MM-DD");
-        const menus = await get(
-          `menus?restaurants=${restaurantIds}&days=${date}&lang=${
-            res.locals.lang
-          }`,
-          1800
-        );
         res.render("area", {
-          title: "Kanttiinit: " + currentArea.name,
-          areas,
-          currentArea,
+          title: "Kanttiinit: " + data.area.name,
+          areas: data.areas,
+          currentArea: data.area,
           isToday: now.isSame(day, "day"),
           isTomorrow: tomorrow.isSame(day, "day"),
-          restaurants: restaurants.sort((a, b) => (a.name > b.name ? 1 : -1)),
-          getMenus: restaurantId => menus[restaurantId][date],
+          restaurants: data.area.restaurants.sort((a, b) =>
+            a.name > b.name ? 1 : -1
+          ),
           day,
           tomorrow: tomorrow.format("YYYY-MM-DD")
         });
       }
     } catch (e) {
       console.log(e);
-      res.status(500).render("index", { error: "Failed to fetch data." });
+      res
+        .status(500)
+        .render("error", { errorMessage: "Failed to fetch data." });
     }
   })
   .get("/:areaId", (req, res) =>
